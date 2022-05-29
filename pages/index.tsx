ae682fcc7678fe93fa86975b1004fa6a2128ef5f
noviarrizal <noviarrizalarif@gmail.com>
2022-05-29 20:18:40 +0800

    fixed a bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,7 +35,7 @@ const Home = ({ netflixOriginals, actionMovies,
     if (loading) return null
 
   return (
-    <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
+    <div className={`relative h-screen bg-gradient-to-b lg:h-[140vh] ${showModal && "!h-screen overflow-hidden"}`}>
       <Head>
         <title>Netflix Clone</title>
         <link rel="icon" href="/favicon.ico" />
